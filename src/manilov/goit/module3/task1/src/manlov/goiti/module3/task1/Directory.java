4f3211bfb139677ed3c8246dcf3bdbdce9bdf132
Манилов Сергей <smanilov86@gmail.com>
2016-06-05 00:44:40 +0300

    create proper packages

---
+++ src/manilov/goit/module3/task1/src/manlov/goiti/module3/task1/Directory.java
@@ -0,0 +1,8 @@
+package manlov.goiti.module3.task1;
+import java.util.List;
+/**
+ * Created by md093 on 05.06.16.
+ */
+public class Directory extends File{
+    private List<File> files;
+}
---
+++ src/manilov/goit/module3/task1/src/manlov/goiti/module3/task1/Picture.java
@@ -0,0 +1,7 @@
+package manlov.goiti.module3.task1;
+
+/**
+ * Created by md093 on 05.06.16.
+ */
+public class Picture {
+}
