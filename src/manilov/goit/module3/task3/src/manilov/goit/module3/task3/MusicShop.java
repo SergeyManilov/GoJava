8bdd43b8a39356131e3e521e8823c95bc7e5abc4
Манилов Сергей <smanilov86@gmail.com>
2016-06-05 01:21:41 +0300

    create proper packages

---
+++ src/manilov/goit/module3/task3/src/manilov/goit/module3/task3/MusicShop.java
@@ -0,0 +1,8 @@
+package manilov.goit.module3.task3;
+import java.util.List;
+/**
+ * Created by md093 on 05.06.16.
+ */
+public class MusicShop extends MusicalInstrument {
+    private List<MusicalInstrument> musicalInstruments;
+}
---
+++ src/manilov/goit/module3/task3/src/manilov/goit/module3/task3/Trumpet.java
@@ -0,0 +1,7 @@
+package manilov.goit.module3.task3;
+
+/**
+ * Created by md093 on 05.06.16.
+ */
+public class Trumpet extends MusicalInstrument {
+}
